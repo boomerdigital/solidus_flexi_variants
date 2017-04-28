c995fb31e4d9624365ae039e4083bc9ed630313c
Allison C. Reilly <acreilly3@gmail.com>
2017-04-28 12:47:44 -0400

    Cleaning up spec_helper support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,29 +12,47 @@ require 'factory_girl'
 require 'ffaker'
 require 'shoulda-matchers'
 require 'pry'
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+require 'capybara/rspec'
+require 'capybara/rails'
 
-require 'solidus_flexi_variants/factories'
+require 'spree/testing_support/preferences'
+require 'spree/testing_support/authorization_helpers'
+require 'spree/testing_support/capybara_ext'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/factories'
+require 'spree/testing_support/url_helpers'
+require 'spree/testing_support/order_walkthrough'
+
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+Dir[File.join(File.dirname(__FILE__), 'factories/*.rb')].each { |f| require f }
+
+
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+  config.include Spree::TestingSupport::Preferences
+  config.include Spree::TestingSupport::UrlHelpers
+  config.include Spree::TestingSupport::ControllerRequests, type: :controller
 
-  # Capybara javascript drivers require transactional fixtures set to false, and we use DatabaseCleaner
-  # to cleanup after each test instead.  Without transactional fixtures set to false the records created
-  # to setup a test will be unavailable to the browser, which runs under a seperate server instance.
-  config.use_transactional_fixtures = false
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  config.infer_base_class_for_anonymous_controllers = false
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
+  config.infer_spec_type_from_file_location!
+  config.mock_with :rspec
   config.order = "random"
   config.color = true
-  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
+  config.fail_fast = ENV['FAIL_FAST'] || false
+  #config.infer_base_class_for_anonymous_controllers = false
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
+  end
+
+  config.before do
+    DatabaseCleaner.strategy = RSpec.current_example.metadata[:js] ? :truncation : :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 end
--- spec/support/capybara.rb
+++
@@ -1,7 +0,0 @@
-require 'capybara/rspec'
-require 'capybara/rails'
-# require 'capybara/poltergeist'
-
-RSpec.configure do |config|
-  # Capybara.javascript_driver = :poltergeist
-end
