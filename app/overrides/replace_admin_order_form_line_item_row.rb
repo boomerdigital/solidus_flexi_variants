5ca82ec5285d8e3372f8bc2e45726fe360b8b428
Allison C. Reilly <acreilly3@gmail.com>
2017-05-01 14:57:35 -0400

    Render options text

--- app/overrides/replace_admin_order_form_line_item_row.rb
+++ app/overrides/replace_admin_order_form_line_item_row.rb
@@ -2,5 +2,19 @@ Deface::Override.new(
   virtual_path: "spree/admin/orders/_line_items",
   name: "add_additional_line_item_fields_partial_to_admin_order_view",
   insert_bottom: ".line-item-name",
-  text: "<%= render partial: 'spree/shared/additional_line_item_fields', locals: {item: item} %>"
+  text: "<%= render partial: 'spree/shared/additional_line_item_fields', locals: { item: item } %>"
+)
+
+Deface::Override.new(
+  virtual_path: "spree/admin/orders/_carton_manifest",
+  name: "add_additional_line_item_fields_partial_to_admin_carton_manifest",
+  insert_bottom: ".item-name",
+  text: "<%= render partial: 'spree/shared/additional_line_item_fields', locals: { item: item } %>"
+)
+
+Deface::Override.new(
+  virtual_path: "spree/admin/orders/_shipment_manifest",
+  name: "add_additional_line_item_fields_partial_to_admin_shipment_manifest",
+  insert_bottom: ".item-name",
+  text: "<%= render partial: 'spree/shared/additional_line_item_fields', locals: { item: item.line_item } %>"
 )
