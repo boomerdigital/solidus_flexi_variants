2e8838fa39aeba46851a11c5e3ed880834cdd03b
Allison Reilly <acreilly3@gmail.com>
2017-05-12 13:52:10 -0400

    Add spec to order contents testing adding products with customizations

--- spec/factories/ad_hoc_option_type_factory.rb
+++ spec/factories/ad_hoc_option_type_factory.rb
@@ -2,5 +2,9 @@ FactoryGirl.define do
   factory :ad_hoc_option_type, class: Spree::AdHocOptionType do
     product
     option_type
+
+    before :create do |ahot|
+      ahot.option_type.option_values << create(:option_value)
+    end
   end
 end
---
+++ spec/models/spree/order_contents_decorator_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+RSpec.describe Spree::OrderContents do
+  let(:order) { create(:order) }
+  let(:variant) { create(:variant, price: 5.00) }
+  subject(:order_contents) { described_class.new(order) }
+
+  describe '#add' do
+    context 'with product customization' do
+      let(:calc) {
+        calc = Spree::Calculator::Engraving.new
+        calc.preferred_price_per_letter = 5
+        calc
+      }
+      let(:product_customization) { build(:product_customization, :with_engraving) }
+
+      before do
+        product_customization.customized_product_options = [build(:customized_product_option, value: 'A', customizable_product_option: Spree::CustomizableProductOption.find_by(name: 'inscription'))]
+        product_customization.product_customization_type.calculator = calc
+      end
+
+      it 'updates the order total price to include the price modifier' do
+        expect{ subject.add(variant, 1, { product_customizations: [product_customization], customization_price: 5.00 }) }.to change { order.total }.from(0.00).to(10.00)
+      end
+
+      it 'attaches product customizations to the line item' do
+        subject.add(variant, 1, { product_customizations: [product_customization], customization_price: 5.00 })
+
+        expect(order.line_items.first.product_customizations.length).to eq(1)
+      end
+    end
+
+    context 'with ad hoc option values' do
+      let(:ad_hoc_option_value) { create(:ad_hoc_option_value, price_modifier: 5.00, option_value: create(:option_value, name: 'small')) }
+
+      it 'updates the order total price to include the price modifier' do
+        expect{ subject.add(variant, 1, { ad_hoc_option_values: [ad_hoc_option_value.id] }) }.to change { order.total }.from(0.00).to(10.00)
+      end
+
+      it 'attaches ad hoc option values to the line item' do
+        subject.add(variant, 1, { ad_hoc_option_values: [ad_hoc_option_value.id] })
+
+        expect(order.line_items.first.ad_hoc_option_values.length).to eq(1)
+      end
+    end
+  end
+end
