9b0daab78782bea8de45a532a5aa257bfa5e7c00
Jeff Mason <masonjeffreys@gmail.com>
2017-12-20 15:53:05 -0500

    RemoveCreateOrderStockLocations

--- app/assets/javascripts/spree/frontend/solidus_flexi_variants.js
+++ app/assets/javascripts/spree/frontend/solidus_flexi_variants.js
@@ -1,5 +1,6 @@
 //= require jquery.formatCurrency-1.4.0.min
 //= require i18n/jquery.formatCurrency.all
+//= require jquery.validate/jquery.validate.min
 //= require_self
 
 Spree.ready(function () {
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,6 +44,9 @@ RSpec.configure do |config|
   config.include Spree::TestingSupport::UrlHelpers
   config.include Spree::TestingSupport::ControllerRequests, type: :controller
 
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   config.infer_spec_type_from_file_location!
   config.mock_with :rspec
   config.order = "random"
