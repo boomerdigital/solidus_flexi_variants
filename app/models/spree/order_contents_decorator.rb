997c94f935cdf580fcc3d7503692fa331617d8fe
Jeff Mason <masonjeffreys@gmail.com>
2018-03-05 10:40:54 -0500

    Check class of options. Handle differently if options is Hash or if ActionController::Parameters. This will avoid the 'with_indifferent_access' access error.

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -14,12 +14,16 @@ Spree::OrderContents.class_eval do
     #### separate options to standard, product_customizations, and add_hoc_option_values
     product_customizations_values = options[:product_customizations]
     ad_hoc_option_value_ids = options[:ad_hoc_option_values]
-    standard_options = options.except(:product_customizations, :ad_hoc_option_values).to_h
+    standard_options = options.except(:product_customizations, :ad_hoc_option_values)
     ######
 
     line_item.quantity += quantity.to_i
-    line_item.options = ActionController::Parameters.new(standard_options).permit(Spree::PermittedAttributes.line_item_attributes).to_h
-    
+
+    if standard_options.class == ActionController::Parameters
+      line_item.options = standard_options.permit(Spree::PermittedAttributes.line_item_attributes).to_h
+    else
+      line_item.options = ActionController::Parameters.new(standard_options).permit(Spree::PermittedAttributes.line_item_attributes).to_h
+    end
 
     ####### This line is added to make solidus_flexi_variants save customizations
     if product_customizations_values != nil || ad_hoc_option_value_ids != nil
