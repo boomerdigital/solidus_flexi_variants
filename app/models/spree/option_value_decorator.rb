d4068a802694e95e531ef69a043c0271d8687343
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2019-09-06 12:47:32 -0500

    Rails 6 updates namely changes to names of files to conform to the new zeitwerk file loader
    TrackerUrl: https://www.pivotaltracker.com/story/show/168732410

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,3 +1,10 @@
-Spree::OptionValue.class_eval do
-  has_many :ad_hoc_option_values, dependent: :destroy
+module Spree
+  module OptionValueDecorator
+
+    def self.prepended(base)
+      base.has_many :ad_hoc_option_values, dependent: :destroy
+    end
+
+    Spree::OptionValue.prepend self
+  end
 end
